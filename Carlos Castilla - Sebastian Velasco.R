5859b8b7cb95fc0cb234f6aeb1dc587dd09e0fca
sebasvelasco353 <sebasvelasco353@hotmail.com>
2017-09-06 04:44:52 -0500

    some scatter plots, histograms and ...

--- Carlos Castilla - Sebastian Velasco.R
+++ Carlos Castilla - Sebastian Velasco.R
@@ -3,10 +3,37 @@ library('ggplot2')
 d1<-read.table("student-mat.csv",sep=",",header=TRUE)
 d2<-read.table("student-por.csv",sep=",",header=TRUE)
 
-#Mezclo los dos datasets
+#bind the two datasets into one
 d3 <- rbind(d1, d2)
 
-ggplot(d3, aes(x=absences, y=sex)) + 
-  geom_point(shape=1)
+#age vs  Dalc by sex
+ggplot(d3, aes(x=age, y=Dalc, color=sex, alpha = 1/10)) + 
+  geom_point() +
+  scale_colour_hue(l=70) + 
+  geom_jitter()
+
+
+#histogram with family relations on x axis with MEAN
+ggplot(d3, aes(x=famrel)) +
+  geom_histogram(binwidth=.5, colour="black", fill="white") +
+  geom_vline(aes(xintercept=mean(famrel, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1)
+
+#histogram with health on x axis with MEAN
+ggplot(d3, aes(x=health)) +
+  geom_histogram(binwidth=.5, colour="black", fill="white") +
+  geom_vline(aes(xintercept=mean(health, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1)
+
+#Dalc vs health colored by sex
+ggplot(d3, aes(x=Dalc, y=health, color=sex, alpha = 1/10)) + 
+  geom_point() +
+  scale_colour_hue(l=70) + 
+  geom_jitter()
+
+# The aesthetic fill also takes different colouring scales
+# setting fill equal to a factor variable uses a discrete colour scale
+k <- ggplot(d3, aes(Dalc, fill = famsup))
+k + geom_bar()
 
 str(d3)
