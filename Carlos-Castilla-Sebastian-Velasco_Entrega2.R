d0eba1c4717e7d93478685c3ebe4ef114c1cea67
sebasvelasco353 <sebasvelasco353@hotmail.com>
2017-10-25 04:29:05 -0500

    Random forest initialized and importance of variables ploted... still need to do a corelation matrix v3

--- Carlos-Castilla-Sebastian-Velasco_Entrega2.R
+++ Carlos-Castilla-Sebastian-Velasco_Entrega2.R
@@ -84,4 +84,4 @@ varImpPlot(randomF)
 #cosas pa corelation matix tuto aqui 
 #http://www.sthda.com/english/wiki/ggplot2-quick-correlation-matrix-heatmap-r-software-and-data-visualization
 cormat <- cor(d)
-ggplot(data = cormat, aes(x=Var1, y=Var2, fill=value)) + geom_tile()
+corrplot(comrat)
