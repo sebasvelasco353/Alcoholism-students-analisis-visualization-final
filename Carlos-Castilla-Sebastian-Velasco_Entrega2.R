35db4b64007c6b7f489f282cff4c59125fa3b46b
Sebastian Velasco <sebasvelasco353@hotmail.com>
2017-10-25 11:00:17 -0500

    Finished the markdown of the prediction

--- Carlos-Castilla-Sebastian-Velasco_Entrega2.R
+++ Carlos-Castilla-Sebastian-Velasco_Entrega2.R
@@ -10,28 +10,28 @@ library('randomForest')
 library('corrplot')
 
 # Antes de empezar con cualquier parte del trabajo seteo un seed.
-set.seed(425)
+set.seed(420)
 
 theme_set(theme_tufte())  # from ggthemes
 
 d1<-read.table("student-mat.csv",sep=",",header=TRUE)
 d2<-read.table("student-por.csv",sep=",",header=TRUE)
 
-#Hacemos la unión de los datasets de los estudiantes de la clase de portuges y matemáticas
+#Hacemos la uni?n de los datasets de los estudiantes de la clase de portuges y matem?ticas
 d3 <- rbind(d1, d2)
 
-#Según el archivo de Kaggle, 382 estudiantes se repiten en los dos datasets. En tanto debemos detectarlos y solo 
-#dejar una instancia. El archivo de la plataforma nos provees las columnas que nos permitirán hacer una clara 
-#identificación a falta de un ID por estudiante
+#Seg?n el archivo de Kaggle, 382 estudiantes se repiten en los dos datasets. En tanto debemos detectarlos y solo 
+#dejar una instancia. El archivo de la plataforma nos provees las columnas que nos permitir?n hacer una clara 
+#identificaci?n a falta de un ID por estudiante
 columnas.identificadoras <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
 
 #Ahora, haciendo uso de la funcion rownames(), que nos permite obtener strings con los nombre de las columnas, y 
-# la función unique(), que nos permite identificar los índices de las columnas que no estén repetidas, podremos saber con exactitud
-#cuales elemntos no se repiten. Ahora, filas.unicas contendrá esta información.
+# la funci?n unique(), que nos permite identificar los ?ndices de las columnas que no est?n repetidas, podremos saber con exactitud
+#cuales elemntos no se repiten. Ahora, filas.unicas contendr? esta informaci?n.
 filas.unicas <- rownames(unique(d3[,as.character(columnas.identificadoras)]))
 
-#ahora, creamos una variable que contiene estas las filas que no están repetidas. 
-#además, excluiremos las columnas G1,G2 y G3 ya que no se pueden considerar como causas sino efectos de los demás clasificadores
+#ahora, creamos una variable que contiene estas las filas que no est?n repetidas. 
+#adem?s, excluiremos las columnas G1,G2 y G3 ya que no se pueden considerar como causas sino efectos de los dem?s clasificadores
 d <-d3[filas.unicas, 1:30]
 
 #comprobaciones de que el dataframe solamente tiene 650 elementos. 
@@ -42,21 +42,21 @@ nrow(d)
 ncol(d)
 colnames(d)
 
-#Ahora bien, nuestras variables objetivo serán Dalc y Wacl. Estas, nos permiten determinar el nivel de consumo de alcohol
-# en día de semana (Dalc) y en fines de semana (Walc) en una escala del 1 al 5. Sin embargo, para propósitos de lograr 
-#resultados satisfactorios con algunos algoritmos como el arbol de decisión se ha convertido en una variable binaria. 
+#Ahora bien, nuestras variables objetivo ser?n Dalc y Wacl. Estas, nos permiten determinar el nivel de consumo de alcohol
+# en d?a de semana (Dalc) y en fines de semana (Walc) en una escala del 1 al 5. Sin embargo, para prop?sitos de lograr 
+#resultados satisfactorios con algunos algoritmos como el arbol de decisi?n se ha convertido en una variable binaria. 
 
-#Dentro del dataset d, se creará una columna llamada DalcBina donde se guardará el valor binario determinado por un condicional 
-#que evalua si el valor es mayor o menor que 3. En caso de ser mayor a 3 se guardará un valor de "yes", de lo contrario "no"
+#Dentro del dataset d, se crear? una columna llamada DalcBina donde se guardar? el valor binario determinado por un condicional 
+#que evalua si el valor es mayor o menor que 3. En caso de ser mayor a 3 se guardar? un valor de "yes", de lo contrario "no"
 d$DalcBina <- ifelse(d$Dalc>3, "yes", "no")
 
-#Dentro del dataset d, se creará una columna llamada WalcBina donde se guardará el valor binario determinado por un condicional 
-#que evalua si el valor es mayor o menor que 3. En caso de ser mayor a 3 se guardará un valor de "yes", de lo contrario "no"
+#Dentro del dataset d, se crear? una columna llamada WalcBina donde se guardar? el valor binario determinado por un condicional 
+#que evalua si el valor es mayor o menor que 3. En caso de ser mayor a 3 se guardar? un valor de "yes", de lo contrario "no"
 d$WalcBina <- ifelse(d$Walc>3, "yes", "no")
 
-# También, se usará una ecuación que nos permite encontrar cuál sería el consumo de alcohol en la totalidad que daría sumar Walc y Dalc. Se
-# le denomina Alc y consiste en multiplicar los días que comprendería Walc y Dalc de la semana sobre el total de estos. Determinando así un valor
-#que es un espectro más amplio de ámbos. Se utiliza la función round() con parámetro digits=0 para obtener numeros redondeados del 1-5 de nuevo.
+# Tambi?n, se usar? una ecuaci?n que nos permite encontrar cu?l ser?a el consumo de alcohol en la totalidad que dar?a sumar Walc y Dalc. Se
+# le denomina Alc y consiste en multiplicar los d?as que comprender?a Walc y Dalc de la semana sobre el total de estos. Determinando as? un valor
+#que es un espectro m?s amplio de ?mbos. Se utiliza la funci?n round() con par?metro digits=0 para obtener numeros redondeados del 1-5 de nuevo.
 d$Alc <- round((d$Walc*2+d$Dalc*5)/7,digits = 0)
 
 #Hago un summary para saber los quartiles, media y mediana de la variable Alc
@@ -83,5 +83,5 @@ varImpPlot(randomF)
 
 #cosas pa corelation matix tuto aqui 
 #http://www.sthda.com/english/wiki/ggplot2-quick-correlation-matrix-heatmap-r-software-and-data-visualization
-cormat <- cor(d)
-corrplot(comrat)
+#cormat <- cor(d)
+#corrplot(comrat)
